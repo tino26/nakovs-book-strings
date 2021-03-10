2a8500532dee6a4997ce8dfa942d5bf146f6cb14
tino26 <aresfrend13@gmail.com>
2021-03-10 11:48:36 +0200

    keyWordsRegex

---
+++ ConsoleApp1/banWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace keyWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] keyWord = Console.ReadLine().Split(',');
+            foreach (var item in keyWord)
+            {
+                Regex rg = new Regex(@"(\b)" + item + @"(\b)?");
+                Match match = rg.Match(input.ToString());
+                input=rg.Replace(input.ToString(), new string('*', item.Length));
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ ConsoleApp1/keyWord/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace keyWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentences = Console.ReadLine().Split('.');
+            string keyWord = Console.ReadLine();
+            Console.WriteLine(string.Join(Environment.NewLine, sentences.Where(x=>Regex.IsMatch(x, @"(\b)"+keyWord+@"(\b)?"))));
+        }
+    }
+}
